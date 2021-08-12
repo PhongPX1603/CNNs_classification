b7c8a348144c5e89eb1256363b1ba2a1fb654f98
PhongPX1603 <phongpx.1603@gmail.com>
2021-08-12 15:42:22 +0700

    add transform.Resize

--- dataset/cifar10.py
+++ dataset/cifar10.py
@@ -5,7 +5,8 @@ def Cifar10():
 	mean = [0.4914005398750305, 0.4821619391441345, 0.44653016328811646]
 	std = [0.1952536255121231, 0.19247283041477203, 0.1941995918750763]
 
-	transform = transforms.Compose([transforms.ToTensor(),
+	transform = transforms.Compose([transforms.Resize(224),
+									transforms.ToTensor(),
 	                                transforms.Normalize(mean, std)])
 
 	train_dataset = datasets.CIFAR10(root='dataset', train=True, transform=transform, download=True)
