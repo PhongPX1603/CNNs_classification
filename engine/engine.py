1b02e09d41c84a9272a8201cfc47bf71c9ad44c5
PhongPX1603 <phongpx.1603@gmail.com>
2021-08-12 11:14:06 +0700

    change code

--- engine/engine.py
+++ engine/engine.py
@@ -1,63 +1,68 @@
-from torch import nn
 import torch
+from torch import nn
 
-def train_epoch(data_loader, model, criterion, optimizer, device='cuda'):
-	model.to(device)
-	model.train()
 
-	accuracies, losses = [], []
+class TrainEpoch(nn.Module):
+    def __init__(self):
+        super(TrainEpoch, self).__init__()
 
-	for samples, targets in data_loader:
-		samples = samples.to(device)
-		targets = targets.to(device)
+    def forward(self, data_loader, model, criterion, optimizer, device):
+        model.to(device)
+        model.train()  # set training mode
 
-		preds = model(samples)
-		loss = criterion(targets, preds)
+        accuracies, losses = [], []
+        for x, y_true in data_loader:
+            x = x.to(device)
+            y_true = y_true.to(device)
+            
+            optimizer.zero_grad()
+            y_pred = model(x)
+            loss = criterion(y_pred, y_true)
+            loss.backward()
+            optimizer.step()
 
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
+            y_pred = nn.Softmax(dim=1)(y_pred)
+            y_pred = y_pred.argmax(dim=1)  # B, num_classes
+            correct = torch.sum(y_true == y_pred)
+            accuracy = correct.item() / y_true.shape[0]
 
-		preds = nn.Softmax(dim=1)(preds)
-		preds = preds.argmax(dim=1)
+            accuracies.append(accuracy)
+            losses.append(loss.item())
 
-		correct = torch.sum(preds == targets)
-		accuracy = correct / targets.shape[0]
+        average_loss = sum(losses) / len(losses) if len(losses) else 0.
+        average_accuracy = sum(accuracies) / len(accuracies) if len(accuracies) else 0.
 
-		accuracies.append(accuracy)
-		losses.append(loss)
+        return average_accuracy, average_loss
 
-	average_loss = sum(losses) / len(losses)
-	average_accuracies = sum(accuracies) / len(accuracies)
 
-	return average_accuracies, average_loss
+class EvalEpoch(nn.Module):
+    def __init__(self):
+        super(EvalEpoch, self).__init__()
 
-def valid_epoch(data_loader, model, criterion, device='cuda'):
-	model.to(device)
-	model.eval()
+    def forward(self, data_loader, model, criterion, device):
+        model.to(device)
+        model.eval()    # set valuating mode
 
-	accuracies, losses = [], []
+        accuracies, losses = [], []
 
-	for samples, targets in data_loader:
-		samples = samples.to(device)
-		targets = targets.to(device)
+        with torch.no_grad():
+            for x, y_true in data_loader:
+                x = x.to(device) 
+                y_true = y_true.to(device)
 
-		preds = model(samples)
-		loss = criterion(preds, targets)
+                y_pred = model(x)
+                loss = criterion(y_pred, y_true)
 
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
+                y_pred = nn.Softmax(dim=1)(y_pred)
+                y_pred = y_pred.argmax(dim=1)  # B, num_classes
 
-		preds = nn.Softmax(dim=1)(preds)
-		preds = preds.argmax(dim=1)
-		correct = torch.sum(preds == targets)
-		accuracy = correct / targets.shape[0]
+                correct = torch.sum(y_true == y_pred)
+                accuracy = correct.item() / y_true.shape[0]
 
-		accuracies.append(accuracy)
-		losses.append(loss)
+                accuracies.append(accuracy)
+                losses.append(loss.item())
 
-	average_loss = sum(losses) / len(losses)
-	average_accuracies = sum(accuracies) / len(accuracies)
+        average_loss = sum(losses) / len(losses) if len(losses) else 0.
+        average_accuracy = sum(accuracies) / len(accuracies) if len(accuracies) else 0.
 
-	return average_accuracies, average_loss
+        return average_accuracy, average_loss
