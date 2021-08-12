4acde6453858529bc8cbf81840cdca94b9cd3765
PhongPX1603 <phongpx.1603@gmail.com>
2021-08-12 11:32:40 +0700

    change code

--- training.py
+++ training.py
@@ -1,48 +1,84 @@
-import argparse
+import copy
 import utils
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import argparse
+from pathlib import Path
+
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--config-path', type=str, default='config/yaml_file.yaml')
-	parser.add_argument('--num-epochs', type=int, default=50)
-	parser.add_argument('--device', type=str, default='cuda')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config-path', type=str, default='config/hymenoptera_training.yaml')
+    parser.add_argument('--num-epochs', type=int, default=50)
+    parser.add_argument('--device', type=str, default='cuda')
+    args = parser.parse_args()
 
-	config = utils.load_yaml(args.config_path)
+    config = utils.load_yaml(args.config_path)
 
-	train_data = utils.create_instance(config['train_dataset'])
-	train_loader = utils.create_instance(config['train_loader'], **{'dataset': train_data})
+    # train dataloader
+    train_dataset = utils.create_instance(config['train_dataset'])
+    train_loader = utils.create_instance(config['train_loader'], **{'dataset': train_dataset})
 
-	valid_data = utils.create_instance(config['valid_dataset'])
-	valid_loader = utils.create_instance(config['valid_loader'], **{'dataset': valid_data})
+    # train eval dataloader
+    train_eval_dataset = utils.create_instance(config['train_eval_dataset'])
+    train_eval_loader = utils.create_instance(config['train_eval_loader'], **{'dataset': train_eval_dataset})
 
-	model = utils.create_instance(config['model'])
+    # valid dataloader
+    valid_dataset = utils.create_instance(config['valid_dataset'])
+    valid_loader = utils.create_instance(config['valid_loader'], **{'dataset': valid_dataset})
 
-	optimizer = utils.create_instance(config['optimizer'], **{'params': model.parameters()})
+    # model
+    model = utils.create_instance(config['model'])
 
-	criterion = utils.create_instance(config['criterion'])
+    # loss function
+    criterion = utils.create_instance(config['criterion'])
 
-	lr_scheduler = utils.create_instance(config['lr_scheduler'], **{'optimizer': optimizer})
+    optimizer = utils.create_instance(config['optimizer'], **{'params': model.parameters()})
+    lr_scheduler = utils.create_instance(config['lr_scheduler'], **{'optimizer': optimizer})
 
-	train_epoch = utils.create_instance(config['trainer'])
-	valid_epoch = utils.create_instance(config['validation'])
+    train_epoch = utils.create_instance(config['trainer'])
+    eval_epoch = utils.create_instance(config['evaluator'])
+
+    train_loss_history = []
+    train_acc_history = []
+
+    train_eval_loss_history = []
+    train_eval_acc_history = []
+
+    valid_loss_history = []
+    valid_acc_history = []
+
+    best_valid_acc = 0.
+    best_model_state_dict = dict()
+    best_optim_state_dict = dict()
 
 
-	best_acc = 0
-	for epoch in range(args.num_epochs):
-		acc_train, loss_train = train_epoch(train_loader, model, criterion, optimizer, args.device)
-		acc_valid, loss_valid = valid_epoch(valid_loader, model, criterion, args.device)
-		print(f'epoch {epoch}:\n- train_loss: {loss_train} - train_acc: {acc_train}')
-		print(f'- valid_loss: {loss_valid} - valid_acc: {acc_valid}')
+    for epoch in range(args.num_epochs):
+        train_acc, train_loss = train_epoch(train_loader, model, criterion, optimizer, args.device)
+        valid_acc, valid_loss = eval_epoch(valid_loader, model, criterion, args.device)
 
-		if acc_valid > best_acc:
-			best_model_state_dict = copy.deepcopy(model.state_dict())
-			best_acc = acc_valid
+        print(f'#Epoch {epoch + 1}:\n- train_loss: {train_loss} - train_accuracy: {train_acc}')
+        print(f'- valid_loss: {valid_loss} - valid_accuracy: {valid_acc}')
 
-	print(f'Best accuracy: {best_acc}')
-	output_dir = Path('weights')
-	if not output_dir.exists():
-		output_dir.mkdir(parents=True)
+        lr_scheduler.step(valid_loss)
 
-	weight_path = output_dir.joinpath(f'best_model_accuracy={best_valid_acc}.pth')
-	torch.save(obj=best_model_state_dict, f=weight_path)
+        if valid_acc > best_valid_acc:
+            best_model_state_dict = copy.deepcopy(model.state_dict())
+            best_optim_state_dict = copy.deepcopy(optimizer.state_dict())
+            best_valid_acc = valid_acc
+
+
+        train_loss_history.append(train_loss)
+        valid_loss_history.append(valid_loss)
+        train_acc_history.append(train_acc)
+        valid_acc_history.append(valid_acc)
+
+    print(f'Best Validation Accuracy: {best_valid_acc:4f}')
+
+    output_dir = Path('weights')
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+
+    weight_path = output_dir.joinpath(f'best_model_accuracy={best_valid_acc}.pth')
+    torch.save(obj=best_model_state_dict, f=weight_path)
