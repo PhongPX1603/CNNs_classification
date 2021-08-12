e244d4baf905a63c9575317bc36e7456a4a4a1ca
PhongPX1603 <phongpx.1603@gmail.com>
2021-08-12 12:09:58 +0700

    fix code

--- dataset/cifar10.py
+++ dataset/cifar10.py
@@ -10,9 +10,9 @@ def Cifar10():
 	                                transforms.Normalize(mean, std)])
 
 	train_dataset = datasets.CIFAR10(root='dataset', train=True, transform=transform, download=True)
-	valide_dataset = datasets.CIFAR10(root='dataset', train=False, transform=transform, download=True)
+	valid_dataset = datasets.CIFAR10(root='dataset', train=False, transform=transform, download=True)
 
 	train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
-	valide_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
+	valid_loader = DataLoader(dataset=valid_dataset, batch_size=64, shuffle=False)
 
 	return train_loader, valid_loader
