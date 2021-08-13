fbfb8142d23a1ccc6682b4b031239489ee2dad9a
PhongPX1603 <phongpx.1603@gmail.com>
2021-08-13 21:55:56 +0700

    fix test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import utils
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=str, default='cuda')
+    args = parser.parse_args()
+
+	config = utils.load_yaml('config/cifar.yaml')
+
+	train_dataset = utils.create_instance(config['train_dataset'])
+	train_loader = utils.create_instance(config['train_loader'])
+
+	valid_dataset = utils.create_instance(config['valid_dataset'])
+	valid_loader = utils.create_instance(config['valid_loader'])
+
+	visualize = utils.create_instance(config['visualize'])
+
+	visualize(train_loader, args.device)
