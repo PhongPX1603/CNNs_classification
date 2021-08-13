bbbfd528f740b058218af16f8faddd8884894577
PhongPX1603 <phongpx.1603@gmail.com>
2021-08-13 22:59:38 +0700

    fix code

--- test.py
+++ test.py
@@ -2,19 +2,20 @@ import utils
 import argparse
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--device', type=str, default='cuda')
-	parser.add_argument('--config-path', type=str, default='config/cifar.yaml')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config-path', type=str, default='config/yaml_file.yaml')
+    parser.add_argument('--device', type=str, default='cuda')
+    args = parser.parse_args()
 
-	config = utils.load_yaml(args.config_path)
+    config = utils.load_yaml(args.config_path)
 
-	train_dataset = utils.create_instance(config['train_dataset'])
-	train_loader = utils.create_instance(config['train_loader'], **{'dataset': train_dataset})
+    # train dataloader
+    train_dataset = utils.create_instance(config['train_dataset'])
+    train_loader = utils.create_instance(config['train_loader'], **{'dataset': train_dataset})
 
-	valid_dataset = utils.create_instance(config['valid_dataset'])
-	valid_loader = utils.create_instance(config['valid_loader'], **{'dataset': valid_dataset})
+    # valid dataloader
+    valid_dataset = utils.create_instance(config['valid_dataset'])
+    valid_loader = utils.create_instance(config['valid_loader'], **{'dataset': valid_dataset})
 
-	visualize = utils.create_instance(config['visualize'])
-
-	visualize(train_loader, args.device)
+    print(len(train_loader))
+    print(len(valid_loader))
