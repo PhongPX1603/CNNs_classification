ea8cc462b053edfd9339efea46d7654473edcc45
PhongPX1603 <phongpx.1603@gmail.com>
2021-08-19 11:53:48 +0700

    change code

--- dataset/cifar10.py
+++ dataset/cifar10.py
@@ -1,17 +1,18 @@
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 
+def Cifar10():
+	mean = [0.4914005398750305, 0.4821619391441345, 0.44653016328811646]
+	std = [0.1952536255121231, 0.19247283041477203, 0.1941995918750763]
 
-mean = [0.4914005398750305, 0.4821619391441345, 0.44653016328811646]
-std = [0.1952536255121231, 0.19247283041477203, 0.1941995918750763]
+	transform = transforms.Compose([transforms.Resize(224),
+									transforms.ToTensor(),
+	                                transforms.Normalize(mean, std)])
 
-transform = transforms.Compose([transforms.Resize(224),
-								transforms.ToTensor(),
-                                transforms.Normalize(mean, std)])
-def train_data():
 	train_dataset = datasets.CIFAR10(root='dataset', train=True, transform=transform, download=True)
-	return train_dataset
+	valid_dataset = datasets.CIFAR10(root='dataset', train=False, transform=transform, download=True)
 
-def valid_data():
-	valid_dataset = datasets.CIFAR10(root='dataset', train=False, transform=transform, download=True)	
-	return valid_dataset
+	train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+	valid_loader = DataLoader(dataset=valid_dataset, batch_size=64, shuffle=False)
+
+	return train_loader, valid_loader
